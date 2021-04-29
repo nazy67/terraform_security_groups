8b1bf9ce33cc43e7138b958ba9882afe9d54c396
nazy67 <nazykh67@gmail.com>
2021-04-28 21:57:29 -0500

    deleted sg_v.1.0(hard.coded)/.gitignore

--- sg_v.1.0(hard.coded)/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "web_sg_id" {
-  value = aws_security_group.web_sg.id
-}
-output "web_sg_name" {
-  value = aws_security_group.web_sg.name
-}
-output "web_sg_arn" {
-  value = aws_security_group.web_sg.arn
-}
