f1746a6fa81371d9fdc17ae358a264317536e71c
nazy67 <nazykh67@gmail.com>
2021-04-28 21:36:00 -0500

    changed name sg_v_1.0\(hard.coded\)

--- sg_v_1.0(hard.coded)/security_group.tf
+++ sg_v_1.0(hard.coded)/security_group.tf
@@ -1,12 +1,3 @@
-# This security group is the most simple version of how you can configure your sg 
-# resource block. But it makes your template not reusable, because everything is
-# hard coded.  If you would like to do some changes, you have to go over all your code
-# and there's a big chance that you make the changes you didnt' mean to. Our mission
-# using IaaC is to automate provisioning our resources and avoid human error. That's why
-# try to make our code reusable, and when we write our code once we don't touch it, 
-# for that we use variables.
-
-# Security group
 resource "aws_security_group" "web_sg" {
   name        = "web_sg"
   description = "Allow TLS inbound traffic"
