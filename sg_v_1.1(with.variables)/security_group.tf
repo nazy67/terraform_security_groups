c7bcee1f6bbc3fc218b6c39f153ff8bb0f153011
nazy67 <nazykh67@gmail.com>
2021-04-28 21:34:07 -0500

    modified sg_v_1.1\(with.variables\)

--- sg_v_1.1(with.variables)/security_group.tf
+++ sg_v_1.1(with.variables)/security_group.tf
@@ -1,10 +1,4 @@
-# This security group is the most simplified version of how you can configure your sg 
-# resource block using variables. Using variables makes your template reusable, where
-# all the given variables can be changed depending on your needs. If you would like to
-# do some changes the only file needs to be adjusted is dev.tfvars.
-
-# Security group 
-resource "aws_security_group" "web_sg" {
+ resource "aws_security_group" "web_sg" {
   name        = "${var.env}_web_sg"
   description = "Allow TLS inbound traffic"
 
